5672c9b509ac092ddfde9041b036587011586190
PhilemonVanDussen <vandussenph709@northwested.org>
2024-11-18 14:30:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
-# Student name
-# Current date
+# PJ VanDussen
+# 11/18/2024
 # Default Values for Parameters Practice
 
 # 8-3 (T-Shirt)
+def make_shirt(shirt_size, shirt_message):
+    print(f'The t-shirt size is {shirt_size} and it says {shirt_message}')
+make_shirt('medium', 'Hi, Im fine')
+make_shirt(shirt_size= 'large', shirt_message= 'Greeting human')
 
 
 
 
 # 8-4 (Large Shirts)
+def make_shirt(shirt_message, shirt_size = 'large'):
+    print(f'The t-shirt size is {shirt_size} and it says {shirt_message}')
+make_shirt('I love Python')
 
+def make_shirt(shirt_size, shirt_message = 'I love Python'):
+    print(f'The t-shirt size is {shirt_size} and it says {shirt_message}')
+make_shirt('large')
+make_shirt('medium')
+make_shirt('small')
 
 
 
 # 8-5 (Cities)
+def describe_city(city_name, country_name = 'United States'):
+    print(f'The city of {city_name} is in {country_name}')
+describe_city('New York')
+describe_city('Denver')
+
+def describe_city(country_name, city_name = 'London'):
+    print(f'The city of {city_name} is in {country_name}')
+describe_city('England')
